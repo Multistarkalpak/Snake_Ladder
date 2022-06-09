7d72fe40a141388066203fec96d3996f5f4c5e1e
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-06-09 22:59:06 +0530

    starting position set to 0

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("==========Welcome To Snake And Ladder Game========");
+
+            Console.WriteLine("Initital Position of Player is 0");
+
+
+        }
+    }
+}
---
+++ SnakeAndLadder/SnakeAndLadder/Start_position.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class Start_position
+    {
+        public int P1Position = 0;
+    }
+}
